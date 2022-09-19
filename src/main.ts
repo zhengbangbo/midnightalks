65c39cecc06baa5bb6ec400c10112f7f4eefba52
Bang-Bo Zheng <zhengbangbo@hotmail.com>
2022-09-19 12:27:50 +0800

    chore(deps): vite-ssg + vite-plugin-pages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
+import routes from '~pages'
 
-createApp(App).mount('#app')
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  { routes },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {
+    // install plugins etc.
+  },
+)
---
+++ src/pages/a.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>AAA</h1>
+</template>
