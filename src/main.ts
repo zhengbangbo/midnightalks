6b58bca034b1d8c8387ccc34f9d07e24fa3e706e
Bang-Bo Zheng <zhengbangbo@hotmail.com>
2022-09-19 13:06:27 +0800

    chore(deps): unplugin + unocss + vueuse + eslint
    * unplugin-vue-components + unplugin-auto-import + @vue/runtime-core
    * unocss + @unocss/transformer-directives
    * @types/node
    * @vueuse/core
    * eslint + @zhengbangbo/eslint-config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
 import routes from '~pages'
+import 'uno.css'
 
 export const createApp = ViteSSG(
-  // the root component
   App,
-  // vue-router options
   { routes },
-  // function to have custom setups
-  ({ app, router, routes, isClient, initialState }) => {
-    // install plugins etc.
-  },
+  // ({ app, router, routes, isClient, initialState }) => {},
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
+import path from 'path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
+import Component from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Unocss from 'unocss/vite'
 
-// https://vitejs.dev/config/
+const pathSrc = path.resolve(__dirname, 'src')
+
 export default defineConfig({
-  plugins: [vue(), Pages()]
+  plugins: [Vue({
+    reactivityTransform: true,
+  }), Pages(), Component({
+    dts: path.resolve(pathSrc, 'typing/components.d.ts'),
+  }), AutoImport({
+    imports: ['vue', 'vue-router', '@vueuse/core'],
+    dirs: [path.resolve(pathSrc, 'composables')],
+    dts: path.resolve(pathSrc, 'typing/components.d.ts'),
+  }), Unocss()],
 })
