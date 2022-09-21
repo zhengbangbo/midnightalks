0f807a8c43ce55ab6e7611542783a731ab8c0fe6
Bang-Bo Zheng <zhengbangbo@hotmail.com>
2022-09-21 16:53:07 +0800

    fix(ts-integration): reactivity transform

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
+/// <reference types="vue/macros-global" />
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
