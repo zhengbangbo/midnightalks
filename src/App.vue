fd7979f654151c01d8008a4035019b3ceb633e00
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-02-12 11:01:02 +0800

    refactor: `useHead` replace script on html
    Avoid recording traffic in the development environment

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { useHead } from '@vueuse/head'
 import BaseLayout from './layout/BaseLayout.vue'
-onMounted(
-  () => {
-  },
+
+const headDefault = {
+  title: '大内密谈',
+  meta: [
+    { name: 'description', content: '快速浏览《大内密谈》的往期精美封面' },
+    { name: 'keywords', content: 'Podcast, Chinese, Music' },
+  ],
+}
+
+const useUmami = {
+  script: [
+    {
+      'src': 'https://u.imzbb.cc/u.js',
+      'async': true,
+      'defer': true,
+      'data-website-id': '18ce7a3b-77df-42cf-aa9f-f7114d0cc053',
+    },
+  ],
+}
+
+// <script async defer data-website-id="18ce7a3b-77df-42cf-aa9f-f7114d0cc053" src="https://u.imzbb.cc/u.js"/>
+useHead(
+  process.env.NODE_ENV === 'production'
+    ? { ...headDefault, ...useUmami }
+    : { ...headDefault },
 )
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import '@unocss/reset/tailwind.css'
 import { createPinia } from 'pinia'
 import 'uno.css'
 import { createApp } from 'vue'
+import { createHead } from '@vueuse/head'
 import lazyPlugin from 'vue3-lazy'
 import { LoadingPlugin } from 'vue-loading-overlay'
 import App from './App.vue'
@@ -18,8 +19,10 @@ if (process.env.NODE_ENV === 'development')
 async function setupApp() {
   const pinia = createPinia()
   const app = createApp(App)
+  const head = createHead()
   await setupRouter(app)
   app.use(pinia)
+  app.use(head)
   app.use(LoadingPlugin)
   app.use(lazyPlugin, {
     loading,
