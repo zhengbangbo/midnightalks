3d2ce20e343579dd62cd4db1c88c675f04051558
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-04-19 19:51:14 +0800

    feat: update ui

--- src/components/DeclarationBanner.vue
+++ src/components/DeclarationBanner.vue
@@ -5,7 +5,7 @@
 <template>
   <footer relative text-center py-6 leading-7>
     <p>
-      本网站非官方网站，由<a color-red href="https://github.com/zhengbangbo">听众</a>开发。有任何建议欢迎与我联系(zhengbangbo@hotmail.com)，我很乐意改进。
+      本网站非官方网站，由<a bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-clip-text text-transparent href="https://github.com/zhengbangbo">听众</a>开发。
     </p>
   </footer>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,6 +10,7 @@ import { fixedCoverUrl } from '../utils'
 const podcastStore = usePodcastStore()
 const episodeData = ref<episode[]>()
 const $loading = useLoading()
+
 onBeforeMount(
   async () => {
     if (!podcastStore.podcastData.items) {
@@ -28,14 +29,14 @@ onMounted(() => {
   router.afterEach(() => { window.scrollTo(0, 0) })
 })
 
-const isDisplay = ref('none')
+const showReturnTop = ref('none')
 
 const handleScroll = useDebounceFn(() => {
   if (window.scrollY > 1000)
-    isDisplay.value = 'block'
+    showReturnTop.value = 'block'
 
   else
-    isDisplay.value = 'none'
+    showReturnTop.value = 'none'
 }, 200)
 
 onMounted(() => {
@@ -65,8 +66,6 @@ const goTo = {
 <template>
   <div flex="~" @scroll="handleScroll">
     <div flex-1 overflow-auto p-3>
-      <!-- <Suspense>
-        <template #default> -->
       <img
         v-for="{ created, itunes_image } in episodeData"
         :id="created.toString()" :key="created.toString()" v-lazy="fixedCoverUrl(itunes_image?.href)"
@@ -74,17 +73,10 @@ const goTo = {
         p-1
         @click="goTo.episodeDetail(created)"
       >
-      <!-- </template>
-        <template #fallback>
-          <div flex="~" justify-center items-center>
-            全速加载
-          </div>
-        </template>
-      </Suspense> -->
     </div>
 
     <div w="38.2%" text-18px lg:text-26px color-white pr-1>
-      <div v-for="{ created, title } in episodeData" :key="created" hover:color-black lg:pl-5 pt-3>
+      <div v-for="{ created, title } in episodeData" :key="created" hover="color-black op-80" lg:pl-5 pt-3>
         <a :href="anchor(created)">{{ title }}</a>
       </div>
     </div>
@@ -92,7 +84,7 @@ const goTo = {
   <div
     z-2
     cursor-pointer class="rd-50%"
-    bg-black hover:bg-blue fixed text-6 lg:text-10 bottom-5 right-5 w-12 lg:w-20 h-12 lg:h-20 text-center :style="{ display: isDisplay }" @click="scrollToTop"
+    bg-black hover:bg-op-30 fixed text-6 lg:text-10 bottom-5 right-5 w-12 lg:w-20 h-12 lg:h-20 text-center :style="{ display: showReturnTop }" @click="scrollToTop"
   >
     <div flex="~" justify-center items-center w-full h-full color-white>
       Top
